e236379b83439348e6876234695d0e36f3e65ff5
aryan130202 <sabhaniaryan8@gmail.com>
2025-08-22 10:27:27 +0530

    Add files via upload

---
+++ aryan small and large comparison.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+
+{
+    int a, b;
+   printf("Enter the value of A:\n");
+   scanf("%d", &a);
+
+
+   printf("Enter the value of B:\n");
+   scanf("%d",&b);
+
+   if(a>b)
+   {
+      printf("A is greater than B");
+
+      }
+      else
+      {
+      printf("B is greater");
+      }
+
+}
